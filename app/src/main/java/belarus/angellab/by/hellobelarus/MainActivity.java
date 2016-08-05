1f0dcfb9e32eee9ff6d3900a198ec64ff48a2cc9
artur.egiazarov <yegiazarov@gmail.com>
2016-08-05 17:37:54 +0300

    Initial commit

---
+++ app/src/main/java/belarus/angellab/by/hellobelarus/MainActivity.java
@@ -0,0 +1,16 @@
+package belarus.angellab.by.hellobelarus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.appat.tracker.Install;
+import co.appat.tracker.VisitApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
